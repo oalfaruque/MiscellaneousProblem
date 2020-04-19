ae971e1dc4d965cea0f5402f6cd381554985e864
oalfaruque <oalfaruque@gmail.com>
2020-04-18 23:49:28 -0400

    Correction

--- src/miscellaneousproblems/SerializationAndDeserializationConcept.java
+++ src/miscellaneousproblems/SerializationAndDeserializationConcept.java
@@ -13,13 +13,13 @@ public class SerializationAndDeserializationConcept {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         TwoNumbers twoNumbers = new TwoNumbers();
 
-        //Serialization
+        //Serialization:(Converting an Object into a File)
         FileOutputStream myFileOutputStream = new FileOutputStream("TwoNumbers.txt");
         ObjectOutputStream myObjOutputStream = new ObjectOutputStream(myFileOutputStream);
         myObjOutputStream.writeObject(twoNumbers);
 
 
-        //Deserialization
+        //Deserialization:(Converting a File into an Object)
         FileInputStream myFileInputStream = new FileInputStream("TwoNumbers.txt");
         ObjectInputStream myObjInputStream = new ObjectInputStream(myFileInputStream);
         TwoNumbers myFile = (TwoNumbers) myObjInputStream.readObject();
